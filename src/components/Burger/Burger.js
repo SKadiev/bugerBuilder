11dcec5c6220bd237cf65a745c4ddf9534741f15
Stojan Kadiev <kadievstojan@gmail.com>
2021-03-12 16:15:04 +0100

    empty ingredients check

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,17 +3,30 @@ import React from 'react';
 import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
-const burger = ( props ) => (
+const burger = ( props ) => {
+    let tranformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+             return [...Array(props.ingredients[igKey])].map((_, i) => {
+               return <BurgerIngredient key={igKey + i} type={igKey} />
+            });
+        }).reduce((arr, el) => {
+            return arr.concat(el);
+        });
+    if (tranformedIngredients.length === 0) {
+        tranformedIngredients = <p>Please start adding ingredients</p>
+    }
+    
+    return (
+    
+          <div className={classes.Burger}>
+             <BurgerIngredient type="bread-top" />
+             {tranformedIngredients}
+             <BurgerIngredient type="bread-bottom" />
+         </div>
+    ) 
+}
 
-    <div className={classes.Burger}>
-        <BurgerIngredient type="bread-top" />
-        <BurgerIngredient type="cheese" />
-        <BurgerIngredient type="meat" />
-        <BurgerIngredient type="cheese" />
 
-        <BurgerIngredient type="bread-bottom" />
-    </div>
-)
    
 
 
