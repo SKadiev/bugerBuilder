cff14401d6ae1c54f5330273b70400bed9b7b6b5
Stojan Kadiev <kadievstojan@gmail.com>
2021-03-12 13:13:50 +0100

    layout style

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import Aux from '../../hoc/Auxliary'
+import Aux from '../../hoc/Auxliary';
+import classes from './Layout.css';
+
 const layout = (props) => (
-    <Aux>
-        <div>Toolbar, SideDrawer, Backdrop</div> 
-        <main>
+    <Aux >
+        <div >Toolbar, SideDrawer, Backdrop</div> 
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Aux>
