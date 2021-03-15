8ab8aae66aa7167c533bfa988c110483c35e6180
Stojan Kadiev <kadievstojan@gmail.com>
2021-03-15 14:47:11 +0100

    navigation items

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 const Toolbar = (props) => {
     return (
@@ -9,7 +10,7 @@ const Toolbar = (props) => {
             <div>MENU</div>
             <Logo />
            <nav>
-               ...
+             <NavigationItems />
            </nav>
 
         </header>
