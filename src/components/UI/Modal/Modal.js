2c9da2cd3c7dabee777b33d92fa11d37a1221b6c
Stojan Kadiev <kadievstojan@gmail.com>
2021-03-15 18:24:11 +0100

    prevent unnecessary OrderSumary render on hiden modal

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from "./Modal.css"
 import Aux from '../../../hoc/Auxilairy'
 import BackDrop from '../BackDrop/BackDrop'
 
-function Modal(props) {
-    return (
-        <Aux>
-            <BackDrop show={props.show} clicked={props.modalClosed} />
-            <div className ={classes.Modal}
-            style={{
-                transform: props.show ? 'translateY(0)' : 'tranlateY(-100vh)',
-                opacity: props.show ? '1' : '0'
-            }}>
-                {props.children}
-            </div>
-        </Aux>
-    );
+class Modal extends Component {
+
+
+    shouldComponentUpdate (nextProps, nextState) {
+        return nextProps.show !== this.props.show  
+    }
+
+    componentDidUpdate () {
+        console.log('Modal did update');
+    }
+
+    render () {
+
+        return (
+            <Aux>
+                <BackDrop show={this.props.show} clicked={this.props.modalClosed} />
+                <div className ={classes.Modal}
+                style={{
+                    transform: this.props.show ? 'translateY(0)' : 'tranlateY(-100vh)',
+                    opacity: this.props.show ? '1' : '0'
+                }}>
+                    {this.props.children}
+                </div>
+            </Aux>
+        );
+    }
 }
 
 export default Modal;
