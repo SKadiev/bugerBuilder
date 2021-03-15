1a0b502b6e70e3ff1bd4d25c3d51cc89b199d9ce
Stojan Kadiev <kadievstojan@gmail.com>
2021-03-15 12:49:32 +0100

    modal closing

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import classes from "./Modal.css"
+import Aux from '../../../hoc/Auxilairy'
+import BackDrop from '../BackDrop/BackDrop'
 
 function Modal(props) {
     return (
-        <div className ={classes.Modal}
-        style={{
-            transform: props.show ? 'translateY(0)' : 'tranlateY(-100vh)',
-            opacity: props.show ? '1' : '0'
-         }}>
-            {props.children}
-        </div>
+        <Aux>
+            <BackDrop show={props.show} clicked={props.modalClosed} />
+            <div className ={classes.Modal}
+            style={{
+                transform: props.show ? 'translateY(0)' : 'tranlateY(-100vh)',
+                opacity: props.show ? '1' : '0'
+            }}>
+                {props.children}
+            </div>
+        </Aux>
     );
 }
 
 export default Modal;
+
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -24,7 +24,7 @@ class BurgerBuilder extends Component {
         }, 
         price : 4,
         purchasable: false,
-        purchasing: false
+        purchasing: false,
     }
 
     updatePurchasable (ingredients) {
@@ -79,6 +79,10 @@ class BurgerBuilder extends Component {
         this.setState({purchasing: true});
     }
 
+    purchasaCancelHandler =  () => {
+        this.setState({purchasing: false});
+    }
+
     render () {
         const disabledInfo = {
             ...this.state.ingredients
@@ -91,7 +95,7 @@ class BurgerBuilder extends Component {
         return (
             
             <Aux>
-                <Modal show={this.state.purchasing}><OrderSummary ingredients={this.state.ingredients} /></Modal>
+                <Modal show={this.state.purchasing} modalClosed={this.purchasaCancelHandler}  ><OrderSummary ingredients={this.state.ingredients}/></Modal>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls 
                     ingredientAdded={this.addIngredientHandler} 
