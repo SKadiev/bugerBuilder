2f37bff0cbe0abf294a1bfb03f1053942e2c38d2
Stojan Kadiev <kadievstojan@gmail.com>
2021-04-09 11:50:32 +0200

    auth form

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -7,6 +7,7 @@ const NavigationItems = () => (
     <ul className={classes.NavigationItems}>
         <NavigationItem exact link="/" >BurgerBuilder</NavigationItem>
         <NavigationItem link="/orders" >Orders</NavigationItem>
+        <NavigationItem link="/auth" >Authenticate</NavigationItem>
 
     </ul>
 )
